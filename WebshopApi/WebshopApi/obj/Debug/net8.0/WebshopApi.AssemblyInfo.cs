81bf10c8bbb026f046ad23225607ee6508bda33f
Fülöp Ervin <fulopferro@gmail.com>
2025-03-26 14:01:29 +0100

    azure

--- WebshopApi/WebshopApi/obj/Debug/net8.0/WebshopApi.AssemblyInfo.cs
+++ WebshopApi/WebshopApi/obj/Debug/net8.0/WebshopApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebshopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604c42855fac4e9028af75d63dfaf56fc3ecfba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1623213ba570485bd702aecb3505c3d095401ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebshopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebshopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
